9d7721ba0c7507cc7878ecf6f6a63c50d0428a09
Hugo <hugo.thunnissen@gmail.com>
2017-03-21 00:45:29 +0100

    Pom.xml uitgebreid voor jetty dependencies en een beginprojectje gemaakt om met jetty te werken

---
+++ photo-slider-backend/src/main/java/nl/amsta09/driver/MainApp.java
@@ -0,0 +1,47 @@
+package nl.amsta09.driver;
+
+/**
+ * Hello world!
+ *
+ */
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+public class MainApp extends AbstractHandler {
+    
+	@Override
+    public void handle( String target,
+                        Request baseRequest,
+                        HttpServletRequest request,
+                        HttpServletResponse response ) throws IOException,
+                                                      ServletException
+    {
+        // Declare response encoding and types
+        response.setContentType("text/html; charset=utf-8");
+
+        // Declare response status code
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        // Write back response
+        response.getWriter().println("<h1>Hello World</h1>");
+
+        // Inform jetty that this request has now been handled
+        baseRequest.setHandled(true);
+    }
+
+    public static void main( String[] args ) throws Exception
+    {
+        Server server = new Server(8080);
+        server.setHandler(new MainApp());
+
+        server.start();
+        server.join();
+    }
+}
