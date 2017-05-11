6697d693145c89d6e075378f5adc1b4f2b57954f
MarcoBergsma <marcobergsma1999@gmail.com>
2017-05-11 10:03:14 +0200

    Auto stash before merge of "master" and "origin/master"

--- photo-slider/src/main/java/nl/amsta09/data/SqlConnector.java
+++ photo-slider/src/main/java/nl/amsta09/data/SqlConnector.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import javax.swing.Action;
 
 public class SqlConnector {
 
@@ -39,15 +40,58 @@ public class SqlConnector {
 
     }
 
-    public void Execute_Insert_Theme(String ThemeName) throws SQLException, ClassNotFoundException {
+    /*
+     * Insert een theme naar de database
+     */
+    public void Execute_Insert_Theme(String Theme_Name) throws SQLException, ClassNotFoundException {
 
 //            Class.forName("com.mysql.jdbc.Driver");
 //            connection = DriverManager.getConnection("jdbc:mysql://localhost/photoslider", "root", "Aapjes-14");
-
         Statement addThemeStatement = connection.createStatement();
         //Insert de gegevens in de database met een unieke ID
-        String sql = ("insert into theme (name) VALUES ('" + ThemeName + "')");
+        String sql = ("insert into theme (name) VALUES ('" + Theme_Name + "')");
 
         addThemeStatement.execute(sql);
+    }
+
+    public void Execute_Insert_Media(String URL, String Media_Name) throws SQLException {
+
+        Statement addMediaStatement = connection.createStatement();
+        //Insert een media file in de database
+        String sql = ("INSERT INTO Media (name, URL) VALUES ('" + Media_Name + "','" + URL + "')");
+
+        addMediaStatement.execute(sql);
+    }
+
+    public void Execute_Activate_Media(String Media_Name) throws SQLException {
+
+        Statement activateMediaStatement = connection.createStatement();
+        //Activeert de gekozen media file
+        String sql = ("UPDATE Media"
+                + "SET active = 'true'"
+                + "WHERE URL ='" + Media_Name + "'");
+
+        activateMediaStatement.execute(sql);
+    }
+
+    public void Execute_Disable_Media(String Media_Name) throws SQLException {
+
+        Statement disableMediaStatement = connection.createStatement();
+        //Activeert de gekozen media file
+        String sql = ("UPDATE Media"
+                + "SET active = 'disable'"
+                + "WHERE URL ='" + Media_Name + "'");
+
+        disableMediaStatement.execute(sql);
+    }
+
+    public void Execute_Activate_Thema(String Theme_Name) throws SQLException {
+
+        Statement activateThemaStatement = connection.createStatement();
+        //Activeert het gekozen thema
+        String sql = ("UPDATE Thema \n"
+                + "SET on/off = 'false'\n"
+                + "WHERE name ='" + Theme_Name + "'");
+
     }
 }
