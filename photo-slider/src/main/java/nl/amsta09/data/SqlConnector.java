b75213e4fe2c9c69d955d66b7ec98b87fb366684
Micha Ferdinandus <Micha.Ferdinandus@hva.nl>
2017-05-11 10:29:00 +0200

    De rest van de queries (Media verwijderen, Thema verwijderen, Soundeffect toevoegen)

--- photo-slider/src/main/java/nl/amsta09/data/SqlConnector.java
+++ photo-slider/src/main/java/nl/amsta09/data/SqlConnector.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.net.URL;
 
 public class SqlConnector {
 
@@ -49,5 +50,38 @@ public class SqlConnector {
         String sql = ("insert into theme (name) VALUES ('" + ThemeName + "')");
 
         addThemeStatement.execute(sql);
+    }
+    /*
+    Verwijdert de gekozen Media uit de database
+    */
+    public void Execute_Delete_Media(URL MediaURL) throws SQLException, ClassNotFoundException{
+        
+        Statement deleteMediaStatement = connection.createStatement();
+        
+        String sql = ("DELETE FROM Media"
+                + "WHERE URL = " + "'" + MediaURL + "'");
+        
+        deleteMediaStatement.execute(sql);
+    }
+    /*
+    Verwijdert de gekozen Thema uit de database
+    */
+    public void Execute_Delete_Theme(int idTheme) throws SQLException, ClassNotFoundException{
+        Statement deleteThemeStatement = connection.createStatement();
+        //Verwijdert de thema uit de tabel Thema
+        String sql = ("DELETE FROM Thema"
+                + "WHERE id = " + idTheme);
+        
+        deleteThemeStatement.execute(sql);
+    }
+    /*
+    Voegt de Soundeffect toe aan de gekozen Foto
+    */
+    public void Execute_Add_Soundeffect_To_Photo(int idPhoto, int idSoundeffect) throws SQLException, ClassNotFoundException{
+        Statement addSoundeffectStatement = connection.createStatement();
+        //Voegt de Soundeffect id toe aan de Foto
+        String sql = ("INSERT INTO Photo VALUES ('" + idPhoto + ", " + idSoundeffect + "')");
+        
+        addSoundeffectStatement.execute(sql);
     }
 }
