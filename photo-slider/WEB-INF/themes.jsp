383db796e0afaa25e91b9331573725bea6a4046b
MarcoBergsma <marcobergsma1999@gmail.com>
2017-05-11 09:36:17 +0200

    Thema pagina/servlet + sqlconnector update
    thema pagina is wat aangepast. Er is een servlet voor thema's toevoegen. 1 Query is aan SqlConnector toegevoegd

--- photo-slider/WEB-INF/themes.jsp
+++ photo-slider/WEB-INF/themes.jsp
@@ -1,9 +1,12 @@
+<?PHP
+header("Refresh:0");
+?>
 <%@ page contentType="text/html;Charset=UTF-8" pageEncoding="UTF-8" %>
 <%@ taglib prefix="" tagdir="/WEB-INF/tags"%>
 
 <%-- 
 
-	Author: Hugo Thunnissen 
+	Author: Hugo Thunnissen, Marco Bergsma
 
 	Via deze view kunnen de thema's beheerd worden
 	
@@ -13,13 +16,44 @@
 
 	<head>
 		<link href="${context}/webroot/style/mainstylesheet.css" rel="stylesheet" type="text/css" media="screen" />
+		<link href="${context}/webroot/style/thematabel.css" rel="stylesheet" type="text/css" />
+		<link href="${context}/webroot/style/themaStyle.css" rel="stylesheet" type="text/css" />
 		<title>Maintain themes</title>
+	
 	</head>
 
 
 	<body>
 		<%@include file="header.jsp"%>
 		<h1>Op deze pagina kunnen de Thema's beheerd worden</h1>
+		<form method="POST" action="${context}/addTheme" enctype="multipart/form-data">			
+			Naam:
+			<input type="text" name="name" id="name"/>
+			<input type="submit" value="voeg_toe" name="voeg_toe" id="voeg_toe" />
+			
+			</br>
+			
+<div id=themaStyle>
+  <select name="Thema's" size="5" id="tabelwidth">
+    <option value="2010" >Fightme1v1_IRL</option>
+    <option value="2011" >Hippopotomonstrosesquippedaliofobie</option>
+    <option value="2012" >2012</option>
+    <option value="2013" >2013</option>
+    <option value="2014" >2014</option>
+    <option value="2010" >2010</option>
+    <option value="2011" >2011</option>
+    <option value="2012" >2012</option>
+    <option value="2013" >2013</option>
+    <option value="2014" >2014</option>
+    <option value="2010" >2010</option>
+    <option value="2011" >2011</option>
+    <option value="2012" >2012</option>
+    <option value="2013" >2013</option>
+    <option value="2014" >2014</option>	
+   </select>
+	</div>
+		</form>
+		
 	</body>
 
 </html>
--- photo-slider/src/main/java/nl/amsta09/data/SqlConnector.java
+++ photo-slider/src/main/java/nl/amsta09/data/SqlConnector.java
@@ -8,28 +8,46 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class SqlConnector {
-	
-	public SqlConnector(){
-		Connection connection = null;
-		Statement hello;
-		ResultSet set;
-		try{
-			connection = DriverManager.getConnection("jdbc:mysql://supersudonator.filmencode.nl/photoslider?"
-					+ "user=PAD&password=fissafissaheey123&useUnicode=true&useJDBCCompliantTimezoneShift=true"
-					+ "&useLegacyDatetimeCode=false&serverTimezone=UTC");
-			hello = connection.createStatement();
-			set = hello.executeQuery("SELECT * FROM theme;");
-			ResultSetMetaData setMeta = set.getMetaData();
-			int columns = setMeta.getColumnCount();
-			
-			while(set.next()){
-				for(int i = 1; i < columns; i++){
-					String value = set.getString(i);
-					System.out.print(value);
-				}
-			}
-		} catch(SQLException e){
-			e.printStackTrace();
-		}
-	}
+
+    Connection connection = null;
+    Statement hello;
+    ResultSet set;
+
+    public SqlConnector() {
+//        Connection connection = null;
+//        Statement hello;
+//        ResultSet set;
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://supersudonator.filmencode.nl/photoslider?"
+                    + "user=PAD&password=fissafissaheey123&useUnicode=true&useJDBCCompliantTimezoneShift=true"
+                    + "&useLegacyDatetimeCode=false&serverTimezone=UTC");
+
+            hello = connection.createStatement();
+            set = hello.executeQuery("SELECT * FROM theme;");
+            ResultSetMetaData setMeta = set.getMetaData();
+            int columns = setMeta.getColumnCount();
+
+            while (set.next()) {
+                for (int i = 1; i < columns; i++) {
+                    String value = set.getString(i);
+                    System.out.print(value);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void Execute_Insert_Theme(String ThemeName) throws SQLException, ClassNotFoundException {
+
+//            Class.forName("com.mysql.jdbc.Driver");
+//            connection = DriverManager.getConnection("jdbc:mysql://localhost/photoslider", "root", "Aapjes-14");
+
+        Statement addThemeStatement = connection.createStatement();
+        //Insert de gegevens in de database met een unieke ID
+        String sql = ("insert into theme (name) VALUES ('" + ThemeName + "')");
+
+        addThemeStatement.execute(sql);
+    }
 }
