86d3f4504ce8a75138dbce04c3dcca2d31312abc
DESKTOP-152HE04\m_fer <micha.ferdinandus@gmail.com>
2017-06-06 12:15:54 +0200

    Een paar println verwijdert

--- photo-slider/src/main/java/nl/amsta09/app/SlideShowController.java
+++ photo-slider/src/main/java/nl/amsta09/app/SlideShowController.java
@@ -106,7 +106,6 @@ public class SlideShowController {
 		this.theme = theme;
 		this.photos = theme.getPhotoList().listIterator();
                 this.musics = theme.getMusicList().listIterator();
-                System.out.println("Dit is de Size: " + theme.getMusicList().size());
 	}
         
         public Theme getTheme(){
